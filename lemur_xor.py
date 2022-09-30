5071daad8bc52c02e212426581e4cd6a6e991927
Noureddine-0 <114105927+Noureddine-0@users.noreply.github.com>
2022-09-30 15:58:54 +0000

    Update lemur_xor.py

--- lemur_xor.py
+++ lemur_xor.py
@@ -2,5 +2,8 @@ import cv2
 
 im1=cv2.imread('flag_7ae18c704272532658c10b5faad06d74.png')
 im2=cv2.imread('lemur_ed66878c338e662d3473f0d98eedbd0d.png')
-add=cv2.add(im1,im2)
-cv2.imshow("addition",add)
+im = im1 +im2
+cv2.imshow("addition",im)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
